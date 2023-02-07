fd392cc0c840a82a1827e212befa29289571c5a6
gi3636 <fenggi123@gmail.com>
2023-02-07 17:23:31 +0800

    fix:修复查看图片不会更换问题

--- src/page-components/question/image-item/ImageItem.tsx
+++ src/page-components/question/image-item/ImageItem.tsx
@@ -11,7 +11,6 @@ interface ImageItemProps {
   onClose?: () => void;
 }
 function ImageItem({ src, width = '100%', height = '100%', onClose }: ImageItemProps) {
-  const [visible, setVisible] = useState(false);
   return (
     <div className={styles.imageItem}>
       <CloseCircleOutlined className={styles.closeBtn} onClick={onClose} />
@@ -21,11 +20,6 @@ function ImageItem({ src, width = '100%', height = '100%', onClose }: ImageItemP
         width={width}
         height={height}
         preview={{
-          visible,
-          src: src,
-          onVisibleChange: (value) => {
-            setVisible(value);
-          },
           mask: (
             <div>
               <EyeOutlineIcon width={20} height={20} color={'#fff'} />
