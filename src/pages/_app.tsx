abe3e490b40dd720c6ff3febf0fbdb503c4f0e73
gi3636 <fenggi123@gmail.com>
2023-08-24 11:10:31 +0800

    fix:添加https

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }) {
         <title>{t.projectName}</title>
         <meta property='og:title' content={t.projectName} key='title' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-        {/*<meta httpEquiv='Content-Security-Policy' content='upgrade-insecure-requests' />*/}
+        <meta httpEquiv='Content-Security-Policy' content='upgrade-insecure-requests' />
         <link rel='icon' href='/favicon.ico' />
         <meta name='description' content='Learn how to build a personal website using Next.js' />
       </Head>
